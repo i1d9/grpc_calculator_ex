bc12c5521541305d302e3d16ed9754648a3b418f
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-21 16:02:23 +0300

    setup grpc server

--- apps/abacus/lib/abacus.ex
+++ apps/abacus/lib/abacus.ex
@@ -3,6 +3,15 @@ defmodule Abacus do
   Documentation for `Abacus`.
   """
 
+  def start(_type, _args) do
+    children = [
+      {GRPC.Server.Supervisor, endpoint: Abacus.Endpoint, port: 50000, start_server: true}
+    ]
+
+    opts = [strategy: :one_for_one, name: Abacus]
+    Supervisor.start_link(children, opts)
+  end
+
   @doc """
   Hello world.
 
