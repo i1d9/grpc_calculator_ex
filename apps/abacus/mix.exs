4de032c1bba40646b30d3841ad0f480eb3c2ade3
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-21 15:13:16 +0300

    added grpc dependencies

--- apps/abacus/mix.exs
+++ apps/abacus/mix.exs
@@ -18,7 +18,7 @@ defmodule Abacus.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :grpc]
     ]
   end
 
@@ -28,6 +28,10 @@ defmodule Abacus.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+
+      {:grpc, "~> 0.7"},
+      {:protobuf, "~> 0.11"},
+      {:google_protos, "~> 0.3.0"}
     ]
   end
 end
