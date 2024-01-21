1cbfdf4a4cdb39ea9bf638ea82efd0160b45f67c
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-21 16:04:20 +0300

    register grpc genserver

--- apps/abacist/lib/abacist.ex
+++ apps/abacist/lib/abacist.ex
@@ -1,7 +1,18 @@
 defmodule Abacist do
+  use Application
+
   @moduledoc """
   Documentation for `Abacist`.
   """
+
+  def start(_type, _args) do
+    children = [
+      Abacist.Calculate
+    ]
+
+    opts = [strategy: :one_for_one, name: Abacist]
+    Supervisor.start_link(children, opts)
+  end
 
   @doc """
   Hello world.
--- apps/abacist/mix.exs
+++ apps/abacist/mix.exs
@@ -17,9 +17,7 @@ defmodule Abacist.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger, :grpc]
-    ]
+    [mod: {Abacist, []}, extra_applications: [:logger, :grpc]]
   end
 
   # Run "mix help deps" to learn about dependencies.
