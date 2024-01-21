4302f526b16d1a1d2312bbec2e2e233220c675f4
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-21 16:04:34 +0300

    added proto generated file

---
+++ apps/abacus/lib/calculator.pb.ex
@@ -0,0 +1,37 @@
+defmodule Abacus.ArithmeticRequest do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
+
+  field :a, 1, type: :int32
+  field :b, 2, type: :int32
+  field :type, 3, type: :string
+end
+
+defmodule Abacus.ArithmeticReply do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
+
+  field :result, 1, type: :int32
+end
+
+defmodule Abacus.Calculator.Service do
+  @moduledoc false
+
+  use GRPC.Service, name: "abacus.Calculator", protoc_gen_elixir_version: "0.12.0"
+
+  rpc :Multiplication, Abacus.ArithmeticRequest, Abacus.ArithmeticReply
+
+  rpc :Addition, Abacus.ArithmeticRequest, Abacus.ArithmeticReply
+
+  rpc :Subtraction, Abacus.ArithmeticRequest, Abacus.ArithmeticReply
+
+  rpc :Division, Abacus.ArithmeticRequest, Abacus.ArithmeticReply
+end
+
+defmodule Abacus.Calculator.Stub do
+  @moduledoc false
+
+  use GRPC.Stub, service: Abacus.Calculator.Service
+end
